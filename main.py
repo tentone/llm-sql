734c2a1b7598760daeb50a9444f2cd2fd637ce47
Tentone <tentone@outlook.com>
2025-04-20 12:41:50 +0100

    Main file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import database
 import config
 import api
@@ -11,12 +12,22 @@ logging.info("Connecting to database")
 db = database.Database(cfg)
 db.connect()
 
+if os.path.exists("structure.txt"):
+    logging.info("Read database structure")
+    f = open("structure.txt", "r")
+    structure = f.read()
+else:
+    logging.info("Generate database structure")
+    structure = db.database_structure()
+    f = open("structure.txt", "w")
+    f.write(structure)
 
+logging.info("Read initial prompt file")
+f = open("prompt.txt", "r")
+initial_prompt = f.read()
 
-structure = db.database_structure()
-print(structure)
-
-# TODO  
+logging.info("Connect to OpenAPI service")
+# TODO
 
 logging.info("Starting API service")
 api = api.API(cfg)
